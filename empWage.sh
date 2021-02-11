c4aefa817ba3e21fcf3160d97959246627341191
prashikjaware <prashikjaware123@gmail.com>
2021-02-11 12:14:53 +0530

    Added: Calculating Wages fot a Month

--- empWage.sh
+++ empWage.sh
@@ -3,17 +3,24 @@
 isPartTime=1
 isFullTime=2
 empRatePerHr=20
-empCheck=$((RANDOM%3))
+numWorkingDays=20
+totalSaiary=0
 
-case $empCheck in
-	$isFullTime)
-		empHrs=8
-		;;
-	$isPartTime)
-		empHrs=4
-		;;
-	*)
-		empHrs=0
-		;;
-esac
-salary=$(( $empHrs * $empRatePerHr ))
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+	empCheck=$((RANDOM%3))
+
+	case $empCheck in
+		$isFullTime)
+			empHrs=8
+			;;
+		$isPartTime)
+			empHrs=4
+			;;
+		*)
+			empHrs=0
+			;;
+	esac
+	salary=$(( $empHrs * $empRatePerHr ))
+	totalSalary=$(( $totalSalary + $salary ))
+done
