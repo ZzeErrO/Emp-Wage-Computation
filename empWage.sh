f4c5b5307090c3ef3b9698d5cc36dcc6006b7e5d
prashikjaware <prashikjaware123@gmail.com>
2021-02-11 10:52:07 +0530

    Added: Calculating Employee Wage

--- empWage.sh
+++ empWage.sh
@@ -1,1 +1,13 @@
-#welcome to the employee wage computation program
+#!/bin/bash -x
+
+isPresent=1
+randomCheck=$((RANDOM%2))
+
+if [ $isPresent -eq $randomCheck ]
+then
+	empRatePerHr=20
+	empHrs=8
+	salary=$(( $empHrs * $empRatePerHr ))
+else
+	salary=0
+fi
